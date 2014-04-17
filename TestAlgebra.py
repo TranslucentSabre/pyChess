56d02b3ad70c058ae1de579d8037eec76fe91a90
Tim Myers <temyers240@gmail.com>
2014-04-17 10:19:47 -0500

    Additional Unittesting Adding some more framework for the Algebraic Parser

--- TestAlgebra.py
+++ TestAlgebra.py
@@ -4,19 +4,51 @@ from Algebra import *
 
 class AlgebraTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.parser = AlgebraicParser()
+   @classmethod
+   def setUpClass(cls):
+      cls.parser = AlgebraicParser()
+      
+   def stringAndClassCrossConversion(self,moveString,moveClass):
+      self.parser.setAlgebraicMove(moveString)
+      self.assertEqual(moveClass, self.parser.getAlgebraicMoveClass())
 
-    def test_CastleString(self):
-        castleString = "0-0-0"
-        castleClass = AlgebraicMove(castle=True)
+      self.parser.setAlgebraicMove(moveClass)
+      self.assertEqual(moveString, self.parser.getAlgebraicMoveString())
 
-        self.parser.setAlgebraicMove(castleString)
+   def test_QueenSideCastle(self):
+      castleString = "0-0-0"
+      castleClass = AlgebraicMove(castle=True)
 
-        self.assertEqual(castleClass, self.parser.getAlgebraicMoveClass())
+      self.parser.setAlgebraicMove(castleString)
+      self.assertEqual(castleClass, self.parser.getAlgebraicMoveClass())
 
+      castleString = "O-O-O"
 
+      self.stringAndClassCrossConversion(castleString,castleClass)
+      
+   def test_KingsideSideCastle(self):
+      castleString = "0-0"
+      castleClass = AlgebraicMove(castle=True, kingside=True)
+
+      self.parser.setAlgebraicMove(castleString)
+      self.assertEqual(castleClass, self.parser.getAlgebraicMoveClass())
+
+      castleString = "O-O"
+
+      self.stringAndClassCrossConversion(castleString,castleClass)
+        
+   def test_BasicPawn(self):
+      moveString = "e3"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Pawn",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
 if __name__ == "__main__":
     unittest.main()
 
