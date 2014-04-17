fe7a6ca1ceb96ee35c114fe9db2d42843db5a42b
Tim Myers <temyers240@gmail.com>
2014-04-17 13:00:54 -0500

    Additional Unittesting I think that I am done with tests in the Algebraic Parser section. Time to move on.

--- TestAlgebra.py
+++ TestAlgebra.py
@@ -9,11 +9,11 @@ class AlgebraTest(unittest.TestCase):
       cls.parser = AlgebraicParser()
       
    def stringAndClassCrossConversion(self,moveString,moveClass):
-      self.parser.setAlgebraicMove(moveString)
-      self.assertEqual(moveClass, self.parser.getAlgebraicMoveClass())
-
       self.parser.setAlgebraicMove(moveClass)
       self.assertEqual(moveString, self.parser.getAlgebraicMoveString())
+      
+      self.parser.setAlgebraicMove(moveString)
+      self.assertEqual(moveClass, self.parser.getAlgebraicMoveClass())
 
    def test_QueenSideCastle(self):
       castleString = "0-0-0"
@@ -37,7 +37,7 @@ class AlgebraTest(unittest.TestCase):
 
       self.stringAndClassCrossConversion(castleString,castleClass)
         
-   def test_BasicPawn(self):
+   def test_PawnBasic(self):
       moveString = "e3"
       moveClass = AlgebraicMove( destination="e3",
                                  piece="Pawn",
@@ -45,6 +45,174 @@ class AlgebraTest(unittest.TestCase):
                                  capture=False, 
                                  promotion="", 
                                  check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_PawnCapture(self):
+      moveString = "dxe3"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Pawn",
+                                 disambiguation="d", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_PawnCapturePromotion(self):
+      moveString = "cxb8=Q"
+      moveClass = AlgebraicMove( destination="b8",
+                                 piece="Pawn",
+                                 disambiguation="c", 
+                                 capture=True, 
+                                 promotion="Queen", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_PawnCaptureMate(self):
+      moveString = "dxe3#"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Pawn",
+                                 disambiguation="d", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=True, 
+                                 mate=True)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_PawnCheck(self):
+      moveString = "f6+"
+      moveClass = AlgebraicMove( destination="f6",
+                                 piece="Pawn",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=True, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_RookBasic(self):
+      moveString = "Ra4"
+      moveClass = AlgebraicMove( destination="a4",
+                                 piece="Rook",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_RookCapture(self):
+      moveString = "Rxe3"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Rook",
+                                 disambiguation="", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_RookCaptureDisambiguation(self):
+      moveString = "R2xb8"
+      moveClass = AlgebraicMove( destination="b8",
+                                 piece="Rook",
+                                 disambiguation="2", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_RookMate(self):
+      moveString = "Re3#"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Rook",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=True, 
+                                 mate=True)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_RookCaptureCheck(self):
+      moveString = "Rxf6+"
+      moveClass = AlgebraicMove( destination="f6",
+                                 piece="Rook",
+                                 disambiguation="", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=True, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+         
+   def test_KnightBasic(self):
+      moveString = "Nc6"
+      moveClass = AlgebraicMove( destination="c6",
+                                 piece="Knight",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_KnightCapture(self):
+      moveString = "Nxe3"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Knight",
+                                 disambiguation="", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_KnightCaptureDisambiguation(self):
+      moveString = "Naxb8"
+      moveClass = AlgebraicMove( destination="b8",
+                                 piece="Knight",
+                                 disambiguation="a", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=False, 
+                                 mate=False)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_KnightMate(self):
+      moveString = "Ne3#"
+      moveClass = AlgebraicMove( destination="e3",
+                                 piece="Knight",
+                                 disambiguation="", 
+                                 capture=False, 
+                                 promotion="", 
+                                 check=True, 
+                                 mate=True)
+                                 
+      self.stringAndClassCrossConversion(moveString,moveClass)
+      
+   def test_KnightCaptureCheck(self):
+      moveString = "Nxf6+"
+      moveClass = AlgebraicMove( destination="f6",
+                                 piece="Knight",
+                                 disambiguation="", 
+                                 capture=True, 
+                                 promotion="", 
+                                 check=True, 
                                  mate=False)
                                  
       self.stringAndClassCrossConversion(moveString,moveClass)
