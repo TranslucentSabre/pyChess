c1aabfb7ea7cf52367c2eb1988abc07ac89b143c
Timothy Myers <temyers240@gmail.com>
2014-07-31 11:11:13 -0500

    Convert all returns for REST API to JSON.

--- chessGUI.py
+++ chessGUI.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
+import json
 from ChessGame import *
 
 game = ChessGame()
@@ -27,65 +28,65 @@ def showPendingBoard():
 def firstMove():
    """Go to the first move in the game"""
    game.firstMove()
-   return "Success"
-   
+   return jsonify(result="Success")
+
 @app.route("/turn/last")
 def lastMove():
    """Go to the last move in the game"""
    game.lastMove()
-   return "Success"
+   return jsonify(result="Success")
    
 @app.route("/turn/next")
 def nextMove():
    """Go to the next move in the game"""
    game.nextMove()
-   return "Success"
+   return jsonify(result="Success")
    
 @app.route("/turn/previous")
 def previousMove():
    """Go to the previous move in the game"""
    game.previousMove()
-   return "Success"
+   return jsonify(result="Success")
 
 @app.route("/turn/string/<turnString>")
 def gotoTurn(turnString):
     game.gotoTurnString(turnString)
-    return "Success"
+    return jsonify(result="Success")
    
 @app.route("/restart")
 def restart():
    """Restart our current game"""
    game.restartGame()
-   return "Success"
+   return jsonify(result="Success")
    
 @app.route("/commit")
 def commitMove():
     game.commitTurn()
-    return "Success"
+    return jsonify(result="Success")
 
 @app.route("/cancel")
 def cancelMove():
     game.cancelTurn()
-    return "Success"
+    return jsonify(result="Success")
 
 @app.route("/move/coord/<first>/<second>")
 @app.route("/move/coord/<first>/<second>/<promotion>")
 def coordMovePromotion(first,second,promotion=""):
    """Move a piece, this function takes two chess coordinates and a Piece letter to use for promotion, the first being the starting square of the piece to move and the second being the ending square of the move. In order to perform a castle move, move the king to the final position required for the castle."""
    if game.twoCoordMove(first,second,promotion):
-      return "Success"
+      return jsonify(result="Success")
    else:
       game.cancelTurn()
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
 
 @app.route("/move/algebra/<move>")
 def algebraMove(move):
    """Move a piece, this function takes one move in algebraic notation."""
    if game.algebraicMove(move):
-      return "Success"
+      return jsonify(result="Success")
    else:
       game.cancelTurn()
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
    
 @app.route("/load")
 @app.route("/load/<fileName>")
@@ -93,9 +94,9 @@ def load(fileName=""):
    """Read all moves from a file and apply them to the current game, if no argument is given use the default import file configured,
 if one is given use the argument as a filename to read a savegame from."""
    if not game.loadSaveFile(fileName):
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
    else:
-      return "Success"
+      return jsonify(result="Success")
 
 @app.route("/save")
 @app.route("/save/<fileName>")
@@ -103,9 +104,9 @@ def save(fileName=""):
    """Write the current game out to a file. This will erase the old savegame file. If no argument is given use the default export file configured,
 if one is given use the argument as a filename to write the savegame to."""
    if not game.writeSaveFile(fileName):
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
    else:
-      return "Success"
+      return jsonify(result="Success")
       
 """Set or read configuration options. The first argument must be one of the following settings:
 import    (read/set default import file)
@@ -115,18 +116,18 @@ location  (read/set the physical location of the player)
 strict    (read/set strict algebraic parsing mode, if True only exactly formed algebraic notation is accepted)"""
 @app.route("/config/<item>")
 def getConfig(item):
-   value = game.getConfigItem(item)
-   if value != None:
-      return value
+   configValue = game.getConfigItem(item)
+   if configValue != None:
+      return jsonify(value=configValue)
    else:
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
 
 @app.route("/config/<item>/<value>")
 def setConfig(item, value):
    if not game.setConfigItem(item, value):
-      return game.lastError
+      return jsonify(result="Error: "+game.lastError)
    else:
-      return "Success"
+      return jsonify(result="Success")
 
 
 if __name__ == "__main__":
