2f151a23f3d697fb53424c379995ff36bc256407
Tim Myers <temyers240@gmail.com>
2015-01-26 13:55:10 -0600

    Attempt at some actual TDD. I am adding a blank file to use to implement real PGN support as well as a test file with one failing test.

---
+++ pychess/app/testPgn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import unittest, pgn
+
+class pgnTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.parser = pgn.PgnParser()
+
+    def test_ImportFormatTagParse():
+        tagString = '[ site  "The Internet" ]'
+        goldenTag = pgn.Tag("site", "The Internet", "string")
+
+        tagClass = self.parser.parseTag(tagString)
+        self.assertEqual(goldenTag, tagClass)
+
+
+if __name__ == "__main__":
+    unittest.main()
