6a4f48f1aa1c4d0a192ba4ffe036fe400265b2c2
Timothy Myers <temyers240@gmail.com>
2017-05-30 09:54:57 -0500

    Add functionality to change debug file. The open/close behaviour may make more sense in the enable/disable function though.

--- pychess/app/Debug.py
+++ pychess/app/Debug.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from sys import stdout
-#TODO: Add ability to write to specified file 
 
 class Debug(object):
 
@@ -8,8 +7,12 @@ class Debug(object):
       self.sections = []
       self.indent = 2
       self.indentString = ""
+      self.errorString = ""
       self.enabled = False
+      self.defaultOutputFile = stdout
       self.outputFile = stdout
+      self.defaultOutputFileName = "STDOUT"
+      self.outputFileName = "STDOUT"
 
    def dprint(self, *printables):
       if self.enabled:
@@ -37,3 +40,29 @@ class Debug(object):
 
    def enableDebug(self, enabledFlag):
       self.enabled = enabledFlag
+
+   def setDefaultOutput(self):
+      return self.setOutputFileName(self.defaultOutputFileName)
+
+   def setOutputFileName(self, outputFileName):
+      if not self.enabled:
+         if self.outputFileName != self.defaultOutputFileName:
+            #Our current file is not STDOUT, we must close it
+            self.outputFile.close()
+         self.outputFileName = outputFileName
+         if self.outputFileName != self.defaultOutputFileName:
+            #We are using a file that is not STDOUT, we must open it
+            try:
+               self.outputFile = open(self.outputFileName, "a");
+            except (OSError,IOError):
+               self.outputFileName = self.defaultOutputFileName
+               self.outputFile = self.defaultOutputFile
+               self.errorStatus = "Cannot open output file. Defaulting to STDOUT"
+               return False
+         else:
+            self.outputFile = self.defaultOutputFile
+         self.errorString = ""
+         return True
+      else
+         self.errorString = "Output file cannot be changed while debug is enabled."
+         return False
